1e7c3b0cf383419da86e0f93fa07a634d84f2579
Severin Klaus <severin.klaus@deptagency.com>
2023-10-03 11:43:11 +0200

    Add typename to document

--- src/lib/StructuredFieldSplitter.ts
+++ src/lib/StructuredFieldSplitter.ts
@@ -1,13 +1,19 @@
 import { Record, StructuredText } from 'datocms-structured-text-utils';
 
 export function splitStructuredField<R1 extends Record = Record, R2 extends Record = R1>(data: StructuredText<R1, R2>) {
-	const result: (StructuredText<Record, R2> | R1)[] = [];
+	const result: (
+		| {
+				__typename: 'StructuredText';
+				content: StructuredText<Record, R2>;
+		  }
+		| R1
+	)[] = [];
 	let currentDocument: StructuredText<Record, R2> | undefined;
 
 	data.value.document.children.forEach((child) => {
 		if (child.type === 'block') {
 			if (currentDocument) {
-				result.push(currentDocument);
+				result.push({ __typename: 'StructuredText', content: currentDocument });
 				currentDocument = undefined;
 			}
 			const block = data.blocks?.find((block) => block.id === child.item);
